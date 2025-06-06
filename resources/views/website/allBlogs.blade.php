4e3a2ca1e6bbf54df91bd3ab3310239f05e4b46a
Gergely Bod <bodge04@gmail.com>
2025-06-06 18:09:17 +0200

    blogs page icons

--- resources/views/website/allBlogs.blade.php
+++ resources/views/website/allBlogs.blade.php
@@ -59,11 +59,35 @@
                     <div class="course-block col-xl-3 col-lg-4 col-md-6 col-sm-12">
                         <div class="inner-box">
                             <div class="image">
-                                <a href="/blog/{{$blog->slug}}"><img class="transition-500ms"
-                                        src="{{ asset('assets/images/gallery/blog/' . $blog->cover_image) }}" alt="Blog képe" /></a>
+                                <a href="/blog/{{ $blog->slug }}"><img class="transition-500ms"
+                                        src="{{ asset('assets/images/gallery/blog/' . $blog->cover_image) }}"
+                                        alt="Blog képe" /></a>
                             </div>
                             <div class="lower-content">
-                                <h5><a href="/blog/{{$blog->slug}}">{{$blog->title}}</a></h5>
+                                <h5><a href="/blog/{{ $blog->slug }}">{{ $blog->title }}</a></h5>
+                                <!-- Social media ikonok -->
+                                <div class="blog-social-icons mt-2">
+                                    @if ($blog->social_share_link && is_array($blog->social_share_link))
+                                        @if (isset($blog->social_share_link['facebook']))
+                                            <a href="{{ $blog->social_share_link['facebook'] }}" target="_blank"
+                                                class="me-2" title="Facebook">
+                                                <i class="fa fa-facebook-f"></i>
+                                            </a>
+                                        @endif
+                                        @if (isset($blog->social_share_link['instagram']))
+                                            <a href="{{ $blog->social_share_link['instagram'] }}" target="_blank"
+                                                class="me-2" title="Instagram">
+                                                <i class="fa fa-instagram"></i>
+                                            </a>
+                                        @endif
+                                        @if (isset($blog->social_share_link['tiktok']))
+                                            <a href="{{ $blog->social_share_link['tiktok'] }}" target="_blank"
+                                                class="me-2" title="TikTok">
+                                                <i class="fa fa-tiktok"></i>
+                                            </a>
+                                        @endif
+                                    @endif
+                                </div>
                             </div>
                         </div>
                     </div>
